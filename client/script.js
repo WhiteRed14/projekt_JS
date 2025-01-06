997aa9a4e352d5c775fc60b2da60e2b8c60a61df
kwerulik <kwerulik@gmail.com>
2025-01-06 14:41:07 +0100

    reservation_form

--- client/script.js
+++ client/script.js
@@ -1,13 +1,18 @@
 //==================================
 // Podświetlanie aktywnej podstrony
-const links = document.querySelectorAll('.grid_iteam a');
-const currentUrl = window.location.href;
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('.grid_iteam a');
+    const currentUrl = new URL(window.location.href); 
 
-links.forEach(link => {
-    if (link.href === currentUrl) {
-        link.classList.add('active'); 
-    }
+    links.forEach(link => {
+        const linkUrl = new URL(link.href); 
+
+        if (linkUrl.pathname === currentUrl.pathname) {
+            link.classList.add('active');
+        }
+    });
 });
+
 //==================================
 
 //==================================
@@ -29,6 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
     details.addEventListener("click", (event) => {
         const target = event.target;
         const type = target.dataset.type;
+        
         if (!type) return;
 
         if (target.classList.contains("btn-increase")) {
@@ -37,10 +43,8 @@ document.addEventListener("DOMContentLoaded", () => {
             counts[type] = Math.max(0, counts[type] - 1);
         }
 
-        // Aktualizuj wyświetlane liczby
         document.getElementById(`${type}-count`).textContent = counts[type];
 
-        // Aktualizuj podsumowanie
         sum.textContent = `${counts.adults} dorośli · ${counts.children} dzieci · ${counts.rooms} pokój/pokoje`;
     });
 
@@ -85,5 +89,15 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log("Zameldowanie: ", checkin);
         console.log("Wymeldowanie: ", checkout);
         console.log("Gośćie: ", guests);
+
+        const queryParams = new URLSearchParams({
+            location,
+            checkin,
+            checkout,
+            guests,
+        }).toString();
+
+        window.location.href = `subpages/stays.html?${queryParams}`;
     })
 });
+
