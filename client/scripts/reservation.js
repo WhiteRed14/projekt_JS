88ef2cf4f6a4e351ae68e3b80d25ff0f6fac2985
kwerulik <kwerulik@gmail.com>
2025-01-09 12:15:43 +0100

    checkinout

--- client/scripts/reservation.js
+++ client/scripts/reservation.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     //ID apartamentu z URL
     const params = new URLSearchParams(window.location.search);
     const apartmentId = params.get("id");
+    const chcekin = params.get("chcekin");
+    const chcekout = params.get("chcekout");
 
 
     // Obsługa wysyłania formularza
