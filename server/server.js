539b419dfb50c83269a39bff5388dd76d4b35908
RedWhite14 <j.konarzewski.467@studms.ug.edu.pl>
2025-01-10 15:23:39 +0100

    serv upd

--- server/server.js
+++ server/server.js
@@ -115,9 +115,9 @@ app.get('/hotels', (req, res) => {
 
 // returning all data regarding a hotel offer
 app.get('/hotelData', (req, res) => {
-    console.log(req.query)
+    //console.log(req.query)
     const id = req.query;
-
+    console.log(id);
     if (!id) {
         return res.status(400).send('Wszystkie pola są wymagane!');
     } else {
